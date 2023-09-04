e588fe3c7b418e609a303c42f14be271ce1f9f84
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-09-03 21:47:48 -0700

    added vercel analytivs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import Nav from '../components/nav/Nav'
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 
 const MyApp: AppType = ({ Component, pageProps }) => {  
   return (
@@ -9,6 +10,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <div className='flex flex-col min-h-screen bg-cyan-50 dark:bg-[#05011a] px-4'>
         <Nav/>
         <Component {...pageProps}/>
+        <Analytics/>
       </div>
     </ThemeProvider>
   )
